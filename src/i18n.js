8982f06b23e5ca1eeb9e0db53986a1bec37e843d
Daniel Szakacs <36070514+DanielSzakacs@users.noreply.github.com>
2025-09-01 15:42:16 +0200

    Add pomodoro timer with cookie-based status

--- src/i18n.js
+++ src/i18n.js
@@ -4,16 +4,22 @@ import { getLanguage } from './settings';
 const messages = {
   en: {
     start: 'Start pomodoro',
+    stop: 'Stop',
+    restart: 'Restart',
     language: 'Language',
     sendMessage: 'Send me message'
   },
   ja: {
     start: 'ポモドーロを開始',
+    stop: '停止',
+    restart: 'リスタート',
     language: '言語',
     sendMessage: 'メッセージを送ってください'
   },
   ru: {
     start: 'Начать помодоро',
+    stop: 'Стоп',
+    restart: 'Перезапуск',
     language: 'Язык',
     sendMessage: 'Отправлять мне сообщения'
   }
